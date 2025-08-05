915aa140c32fe04f8210a49a6f8cf5baa2f22288
santur123 <132247484+santur123@users.noreply.github.com>
2025-08-05 18:01:24 +0530

    glue_job.py

--- scripts/glue_job.py
+++ scripts/glue_job.py
@@ -1,18 +1,33 @@
 import sys
+from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init("yelp-data-processing", {})
+job.init(args['JOB_NAME'], args)
 
-# Read CSV from S3
-df = spark.read.option("header", "true").csv("s3://finalyelp/finalyelpdata/yelp.csv")
+# Set input and output paths
+input_path = "s3://yelpdatasetsanthosh/yelp_data/"
+output_path = "s3://yelpdatasetsanthosh/yelp_data_final/"
 
-# Write as a single CSV file
-df.coalesce(1).write.mode("overwrite").option("header", "true").csv("s3://yelpdatasetsanthosh/yelp_data/")
+# Read the already transformed data (assumed CSV with header)
+df = spark.read.option("header", True).csv(input_path)
+
+# Optionally drop duplicates if needed
+# df = df.dropDuplicates()
+
+# Write the data to another location
+df.coalesce(1) \
+  .write \
+  .mode("overwrite") \
+  .option("header", True) \
+  .csv(output_path)
 
 job.commit()
